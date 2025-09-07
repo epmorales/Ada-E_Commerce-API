05591c4aa2feda16c832bece6a3955827bb197af
Eric Morales <ericmorales2009@hotmail.comm>
2025-09-07 20:35:49 -0300

    Atualização das classes no pacote repositório

--- src/notificacao/EmailNotificacao.java
+++ src/notificacao/EmailNotificacao.java
@@ -3,4 +3,10 @@ package notificacao;
 public class EmailNotificacao implements Notificacao {
     private final String email;
 
+    public EmailNotificacao(String email) { this.email = email; }
+
+    @Override
+    public void enviarMensagem(String mensagem) {
+        System.out.println("📧 Enviando e-mail para " + email + ": " + mensagem);
+    }
 }
