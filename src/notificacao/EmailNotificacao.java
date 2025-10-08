dc9b8750ff8d714c5f6b779f280b67b582db5fbf
Eric Morales <ericmorales2009@hotmail.comm>
2025-10-08 04:39:28 -0300

    atualizacao classe EmailNotificacao

--- src/notificacao/EmailNotificacao.java
+++ src/notificacao/EmailNotificacao.java
@@ -1,12 +1,24 @@
 package notificacao;
 
+import java.util.concurrent.CompletableFuture;
+
 public class EmailNotificacao implements Notificacao {
     private final String email;
 
-    public EmailNotificacao(String email) { this.email = email; }
+    public EmailNotificacao(String email) {
+        this.email = email;
+    }
 
     @Override
     public void enviarMensagem(String mensagem) {
-        System.out.println("📧 Enviando e-mail para " + email + ": " + mensagem);
+        CompletableFuture.runAsync(() -> {
+            System.out.println(" 📧  [Notificação] Enviando e-mail para "
+                    + email + ": " + mensagem);
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
     }
 }
